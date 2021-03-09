55e6d721e1bf758eabaea9c4ab95fd6d09c8df35
William Allaire <this@that.com>
2021-03-08 20:57:38 -0500

    Initial commit

---
+++ AlgoLongestRange/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoLongestRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] test1 = { 1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6 };
+			int[] result = LargestRange(test1);
+
+			Console.WriteLine($"Range: {result[0]}, {result[1]}");
+        }
+
+		public static int[] LargestRange(int[] array)
+		{
+			Dictionary<int, bool> numbers = new Dictionary<int, bool>();
+			int lowRange;
+			int highRange;
+			int[] range = new int[2];
+
+			if (array.Length == 1)
+				return new int[] { array[0], array[0] };
+
+			// Store the numbers we have in our array to compare
+			// against our left right expansion tests
+			foreach (int num in array)
+			{
+				if (numbers.ContainsKey(num))
+					continue;
+				numbers.Add(num, true);
+			}
+
+			foreach (int num in array)
+			{
+				// Mark that we've checked this number
+				if (numbers.ContainsKey(num))
+					numbers[num] = false;
+
+				lowRange = num;
+				while (numbers.ContainsKey(lowRange))
+				{
+					lowRange--;
+				}
+				lowRange++;
+
+				highRange = num;
+				while (numbers.ContainsKey(highRange))
+				{
+					highRange++;
+				}
+				highRange--;
+
+				if (((highRange - lowRange) + 1) > ((range[1] - range[0]) + 1))
+				{
+					range[0] = lowRange;
+					range[1] = highRange;
+				}
+			}
+
+			return new int[] { range[0], range[1] };
+		}
+	}
+}
